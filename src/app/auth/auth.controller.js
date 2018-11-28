1c4a53a391f5d9c680f5522ebc2e8d9c2e7aba71
Dat Nguyen <symp4t1czz@gmail.com>
2018-11-28 22:45:42 +0100

    creating login function

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -5,9 +5,17 @@ export class AuthController {
 
     this.$auth = $auth;
   }
+
   register() {
     var vm = this;
     this.$auth.signup(this.user).then(function(token){
+      vm.$auth.setToken(token);
+    });
+  }
+
+  login() {
+    var vm = this;
+    this.$auth.login(this.login.user).then(function(token){
       vm.$auth.setToken(token);
     });
   }
